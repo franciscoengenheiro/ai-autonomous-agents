79c2d78029f4bb1bbc5117b9b529ab2727bc08b2
Francisco Engenheiro <a49428@alunos.isel.pt>
2024-04-23 23:07:46 +0100

    Starts report project part 2 chapter

--- iasa49428/iasa_agente/src/lib/ecr/estimulo.py
+++ iasa49428/iasa_agente/src/lib/ecr/estimulo.py
@@ -3,12 +3,11 @@ from abc import ABC, abstractmethod
 class Estimulo(ABC):
 
     """
-    Define a informação que é extraída de uma percepção, de forma a ser utilizada na ativação de comportamentos. Vários estimulos podem ser ativados por uma única percepção.
+    Define a informação que é extraída de uma percepção, de forma a ser utilizada na ativação de respostas. Vários estimulos podem ser ativados por uma única percepção.
     """
-    
+
     @abstractmethod
     def detectar(self, percepcao):
         """
         A deteção de um estimulo consiste em extraír informação relevante de uma percepção (e.g., intensidade).
         """
-    
